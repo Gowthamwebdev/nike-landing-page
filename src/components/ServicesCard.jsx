c9799c4157e92a7685d29daf7bf35f25de2c313e
gowtham <gowthamgalaxy007@gmail.com>
2024-06-07 22:32:59 +0530

    style addded

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -1,6 +1,6 @@
 export const ServicesCard = ({imgURL, label, subtext}) => {
   return (
-    <div className="flex flex-col  sm:w-[350px] sm:min-w-[350px] w-full lg:gap-[1.5rem] shadow-3xl px-10 py-16">
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
       <div className="items-center w-11 h-11 bg-coral-red justify-center flex rounded-full">
       <img src={imgURL} width={24} height={24} alt={label} />
       </div>
