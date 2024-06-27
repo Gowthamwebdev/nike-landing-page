f4e1298aae2aed4a080dafd4c524ea8f14ecdc0b
Gowtham <137925188+Gowthamwebdev@users.noreply.github.com>
2024-06-27 11:26:41 +0530

    footer section refactored entirely

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,55 +1,76 @@
-import React from "react";
-import { headerLogo } from "../assets/images";
-import { Medias, Navitems, products } from "../utils/Data";
+import { copyrightSign } from "../assets/icons";
+import { footerLogo } from "../assets/images";
+import { footerLinks, Medias } from "../utils/Data";
 
 export const Footer = () => {
   return (
-    <section className="max-container items-center justify-center flex">
-      <div className=" flex flex-col gap-6">
-        <div>
-          <img src={headerLogo} alt="" className="mb-6" />
-          <p className="max-w-lg info-text text-slate-200">
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img
+              src={footerLogo}
+              alt="logo"
+              width={150}
+              height={46}
+              className="m-0"
+            />
+          </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
             Get shoes ready for the new term at your nearest Nike store. Find
             Your perfect Size In Store. Get Rewards
           </p>
+          <div className="flex items-center gap-5 mt-8">
+            {Medias.map((icon) => (
+              <div
+                className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+                key={icon.label}
+              >
+                <img
+                  src={icon.imgURL}
+                  alt={icon.label}
+                  width={24}
+                  height={24}
+                />
+              </div>
+            ))}
+          </div>
         </div>
 
-        <div className="flex flex-row gap-[5rem] cursor-pointer ">
-          <ul>
-            {Medias.map((item) => {
-              return (
-                <li className="" key={item.label}>
-                  <div className=" inline-flex text-slate-50 gap-2">
-                    {" "}
-                    <img className=" " src={item.imgURL} alt={item.label} />
-                    <a href={item.href}>{item.label}</a>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-
-          <ul>
-            {products.map((product) => {
-              return (
-                <li className="" key={product.name}>
-                  <div className=" inline-flex text-slate-50 gap-2">
-                    <a href={product.href}>{product.name}</a>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-          <ul className=" text-slate-100">
-            {Navitems.map((item) => {
-              <li key={item.label}>
-                <a href={item.href}>{item.label}</a>
-              </li>;
-            })}
-          </ul>
-          <div></div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section.title}>
+              <h4 className="font-montserrat text-2xl leading-normal font-medium mb-6 text-white">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    className="mt-3 font-montserrat text-base leading-normal text-white-400 hover:text-slate-gray"
+                    key={link.name}
+                  >
+                    <a href={link.link}>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
-    </section>
+
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img
+            src={copyrightSign}
+            alt="copyright sign"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p>Copyright. All rights reserved.</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
+      </div>
+    </footer>
   );
 };
